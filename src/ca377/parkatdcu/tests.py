f722cdb0fae80551bfcd4f63f8fbd0ae8c62abda
Jennifer Foster <Jennifer.Foster@dcu.ie>
2020-10-12 10:50:45 +0100

    Add test case

--- src/ca377/parkatdcu/tests.py
+++ src/ca377/parkatdcu/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class SetUpTest(TestCase):
+
+    def test_welcome(self):
+      """
+      The index page loads and an appropriate welcome message is displayed
+      """
+      response = self.client.get(reverse('parkatdcu:index'))
+      self.assertEqual(response.status_code, 200)
+      self.assertContains(response, "Welcome to ParkAtDCU")
