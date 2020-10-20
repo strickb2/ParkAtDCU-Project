0f3cf9b410e7a9fc30fe162d9328300d8ea5f40f
Jennifer Foster <Jennifer.Foster@dcu.ie>
2020-10-20 21:41:10 +0100

    A1 starter code

--- src/ca377/ca377/settings.py
+++ src/ca377/ca377/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '^%@_k^hiky#xvay9lalw*%20e_uu#gpw!$ci9otyp$yq)nfq_g'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['containers.computing.dcu.ie','127.0.0.1']
 
 
 # Application definition
--- src/ca377/parkatdcu/views.py
+++ src/ca377/parkatdcu/views.py
@@ -3,4 +3,10 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,"parkatdcu/index.html",{})
+
+    context = {}
+
+    # retrieve carpark information from the database
+    # add it to the context dictionary
+    
+    return render(request,"parkatdcu/index.html",context)
