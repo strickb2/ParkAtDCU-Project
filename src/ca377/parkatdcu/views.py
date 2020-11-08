936ec7611ab1b1fd6000a8d7211602f393af88c8
Jennifer Foster <Jennifer.Foster@dcu.ie>
2020-11-08 21:49:57 +0000

    A1 Solution

--- src/ca377/parkatdcu/views.py
+++ src/ca377/parkatdcu/views.py
@@ -1,12 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Carpark, Campus
+
 # Create your views here.
 def index(request):
 
-    context = {}
+    # this is all that is needed for Part 1
+    #carparks = Carpark.objects.all()
+    #context = {'carparks':carparks}
 
-    # retrieve carpark information from the database
-    # add it to the context dictionary
-    
+
+    # Part 2
+    carpark_dict = {}
+    # retrieve  information from the database
+    campuses = Campus.objects.all()
+    for campus in campuses:
+        carparks = Carpark.objects.filter(campus_id=campus) # find all carparks for a campus
+        carpark_dict[campus.name] = carparks
+
+    # add information to the context dictionary
+    context = {'carparkinfo':carpark_dict}
+
     return render(request,"parkatdcu/index.html",context)
