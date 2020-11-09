b1461bd39fc4da9ab87eb6ca0272a329b009a4a6
strickb2 <minzckx6>
2020-11-09 14:11:26 +0000

    Code for A2

--- src/ca377/parkatdcu/views.py
+++ src/ca377/parkatdcu/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 from .models import Carpark, Campus
 
 # Create your views here.
@@ -7,11 +8,21 @@ def index(request):
     carpark_data = Carpark.objects.all()
     campus_data = Campus.objects.all()
     nullcarparks = Campus.objects.filter(campus=None)
+    json = []
+    for park in carpark_data:
+        url = "https://jfoster.pythonanywhere.com/carparks/" + str(park)
+        res = requests.get(url)
+        json_data = res.json()
+        if "error_msg" in json_data:
+            json.append([park, "real time info not available"])
+        else:
+            json.append([park, str(json_data["spaces_available"]) + " spaces available"])
 
     context = {
         "carpark_data" : carpark_data,
         "campus_data" : campus_data,
-        "nullcarparks" : nullcarparks
+        "nullcarparks" : nullcarparks,
+        "json" : json
     }
 
     # retrieve carpark information from the database
