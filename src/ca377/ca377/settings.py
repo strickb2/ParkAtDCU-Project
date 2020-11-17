2155f5823c195d9972809610536331c9976d2f9d
Jennifer Foster <Jennifer.Foster@dcu.ie>
2020-11-17 22:07:08 +0000

    A3 starter code

--- src/ca377/ca377/settings.py
+++ src/ca377/ca377/settings.py
@@ -27,6 +27,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['containers.computing.dcu.ie','127.0.0.1']
 
+# COMMENT THIS OUT IF YOU ARE RUNNING YOUR SERVER LOCALLY
+# IF YOU ARE USING THE DJANGO CONTAINER, REPLACE jfosterdculab WITH YOUR USERNAME
+# USE_X_FORWARDED_HOST = True
+# FORCE_SCRIPT_NAME = '/jfosterdculab'
+
 
 # Application definition
 
--- src/ca377/parkatdcu/views.py
+++ src/ca377/parkatdcu/views.py
@@ -1,25 +1,46 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-from .models import Carpark, Campus
+from .models import Campus, Carpark
+import requests
 
 # Create your views here.
 def index(request):
-
-    # this is all that is needed for Part 1
-    #carparks = Carpark.objects.all()
-    #context = {'carparks':carparks}
-
-
-    # Part 2
-    carpark_dict = {}
-    # retrieve  information from the database
-    campuses = Campus.objects.all()
-    for campus in campuses:
-        carparks = Carpark.objects.filter(campus_id=campus) # find all carparks for a campus
-        carpark_dict[campus.name] = carparks
-
-    # add information to the context dictionary
-    context = {'carparkinfo':carpark_dict}
-
+    context = {}
     return render(request,"parkatdcu/index.html",context)
+
+def carparks(request):
+    context = {}
+    webservice_base_url = "http://jfoster.pythonanywhere.com/carparks/"
+
+    campus_name = request.GET['campus']
+
+    campus = Campus.objects.get(name=campus_name)
+
+    carparks = Carpark.objects.filter(campus_id=campus)
+
+
+    carpark_info = []
+    for carpark in carparks:
+
+        webservice_url = webservice_base_url + carpark.name
+
+        realtime_info = requests.get(webservice_url).json()
+
+        if 'spaces_available' in realtime_info:
+            spaces_available = realtime_info['spaces_available']
+        else:
+            spaces_available = 'not available'
+
+        carpark_info.append({
+                             'name': carpark.name,
+                             'spaces': carpark.spaces,
+                             'disabled_spaces': carpark.disabled_spaces,
+                             'spaces_available': spaces_available
+                             }
+                            )
+
+    context['campus'] = campus_name
+    context['carparks'] = carpark_info
+
+    return render(request,"parkatdcu/carparks.html",context)
