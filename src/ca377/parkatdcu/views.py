aff95ffe8273fd9919f194a8a2290682e52a55ac
strickb2 <minzckx6>
2020-11-18 14:14:54 +0000

    Revert "Code for A2"
    This reverts commit b1461bd39fc4da9ab87eb6ca0272a329b009a4a6.

--- src/ca377/parkatdcu/views.py
+++ src/ca377/parkatdcu/views.py
@@ -1,45 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-import requests
 from .models import Carpark, Campus
 
-from .models import Campus, Carpark
-import requests
-
 # Create your views here.
+def index(request):
+    carpark_data = Carpark.objects.all()
+    campus_data = Campus.objects.all()
+    nullcarparks = Campus.objects.filter(campus=None)
 
-def carparks(request):
-    context = {}
-    webservice_base_url = "http://jfoster.pythonanywhere.com/carparks/"
+    context = {
+        "carpark_data" : carpark_data,
+        "campus_data" : campus_data,
+        "nullcarparks" : nullcarparks
+    }
 
-    campus_name = request.GET['campus']
-
-    campus = Campus.objects.get(name=campus_name)
-
-    carparks = Carpark.objects.filter(campus_id=campus)
-
-
-    carpark_info = []
-    for carpark in carparks:
-
-        webservice_url = webservice_base_url + carpark.name
-
-        realtime_info = requests.get(webservice_url).json()
-
-        if 'spaces_available' in realtime_info:
-            spaces_available = realtime_info['spaces_available']
-        else:
-            spaces_available = 'not available'
-
-        carpark_info.append({
-                             'name': carpark.name,
-                             'spaces': carpark.spaces,
-                             'disabled_spaces': carpark.disabled_spaces,
-                             'spaces_available': spaces_available
-                             }
-                            )
-
-    context['campus'] = campus_name
-    context['carparks'] = carpark_info
-
-    return render(request,"parkatdcu/carparks.html", context)
+    # retrieve carpark information from the database
+    # add it to the context dictionary
+    
+    return render(request,"parkatdcu/index.html",context)
