4022de484aa62fec38a97b764a19f220b47a9a31
strickb2 <minzckx6>
2020-11-29 15:33:37 +0000

    Code for A4 - Version 1

--- src/ca377/ca377/urls.py
+++ src/ca377/ca377/urls.py
@@ -14,9 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('parkatdcu/',include(('parkatdcu.urls','parkatdcu'),namespace='parkatdcu')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += staticfiles_urlpatterns()
